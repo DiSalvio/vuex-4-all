999c82fb4da07bb884acd4a482a08fb7af8eea30
DiSalvio <stephendisalvio@protonmail.com>
2022-04-03 10:57:10 +0800

    create shopping-cart project and start with basic product list component using mocked api

---
+++ shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="product in products" :key="product.id">
+    <h1>{{ product.title }}</h1>
+    <h2>{{ product.price }}</h2>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop.js'
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  created () {
+    shop.getProducts(products => {
+      this.products = products
+    })
+  }
+}
+</script>
